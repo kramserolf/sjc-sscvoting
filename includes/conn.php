4b51f4b26d22e30cc02c12033fd5135d7f4e122a
Kram Serolf <51314960+kramserolf@users.noreply.github.com>
2021-09-15 22:43:57 +0800

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,12 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+	$active_group = 'default';
+	$query_builder = TRUE;
+	$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
