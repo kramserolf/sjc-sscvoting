dcde9ce7c1478cf47b59058f90d93109579f1200
kramserolf <luffyking482@gmail.com>
2021-09-15 23:35:01 +0800

    change database connection string

--- includes/conn.php
+++ includes/conn.php
@@ -1,14 +1,6 @@
 <?php
 
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-
-	$conn = new mysqli($cleardb_username, $cleardb_username, $cleardb_password , $cleardb_db);
+	$conn = new mysqli('us-cdbr-east-04.cleardb.com', 'bff5878dc5fd72', '1a4710b4' , 'heroku_781dc40839e5c9e');
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
