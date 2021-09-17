3fcf1a5a0859513746c3a5c6cbf6a24cf612dc73
kramserolf <luffyking482@gmail.com>
2021-09-17 08:53:18 +0800

    fix db

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = new mysqli('us-cdbr-east-04.cleardb.com', 'bfe52e478df8c5', '06ecd7f4' , 'heroku_8a52fffa867f0cdxx`');
+	$conn = new mysqli('us-cdbr-east-04.cleardb.com', 'bfe52e478df8c5', '06ecd7f4' , 'heroku_8a52fffa867f0cdxx');
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
