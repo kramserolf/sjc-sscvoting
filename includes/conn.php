2b0f0d292b6d64fa64f48afe591de36fa66e4069
kramserolf <luffyking482@gmail.com>
2021-09-17 18:17:17 +0800

    stop voting

--- includes/conn.php
+++ includes/conn.php
@@ -1,12 +1,12 @@
 <?php
 
-// $conn = new mysqli('nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/q4css3k6a0n2jdgi', 'h8a7blgumbf6rcjw', 'w20lpe49k7n6js1v' , 'q4css3k6a0n2jdgi');
-// if ($conn->connect_error) {
-// 	die("Connection failed: " . $conn->connect_error);
-// }
+$conn = new mysqli('nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/q4css3k6a0n2jdgi', 'h8a7blgumbf6rcjw', 'w20lpe49k7n6js1v' , 'q4css3k6a0n2jdgi');
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
 
-	$conn = new mysqli('localhost', 'root', '' , 'votesystem');
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
+	// $conn = new mysqli('localhost', 'root', '' , 'votesystem');
+	// if ($conn->connect_error) {
+	//     die("Connection failed: " . $conn->connect_error);
+	// }
 ?>
